53a65297e521a6fe212699c47d01ff5eb9cb3855
Samuel Brook <samuelbhbrook@gmail.com>
2021-10-13 16:30:05 +0200

    Edit index.html styles.css scripts.js

--- scripts.js
+++ scripts.js
@@ -21,5 +21,91 @@ addBookToLibrary(theSilmarillion);
 console.log(myLibrary);
 
 for (let i = 0; i < myLibrary.length; i++) {
+    displayBookCard(myLibrary[i]);
+}
+
+
+
+
+function displayBookCard(book) {
+    const bookContainer = document.querySelector("#container");
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+    bookContainer.appendChild(bookCard);
+
+    createTitle(bookCard, book);
+
+    addPages(bookCard, book);
+    addReadStatus(bookCard, book);
+
+    addAuthor(bookCard, book);
+
+
+    
+    
+
+
+}
+
+function createTitle(bookCard, book) {
+    let bookTitle = document.createElement("div");
+    bookTitle.classList.add("book-title");
+    bookCard.appendChild(bookTitle);
+    bookTitle.textContent = book.title;
+}
+
+function addAuthor(bookCard, book) {
+    let bookAuthor = document.createElement("div");
+    bookAuthor.classList.add("book-author");
+    bookCard.appendChild(bookAuthor);
+    bookAuthor.textContent = `By ${book.author}`;
+}
+
+function addPages(bookCard, book) {
+    let bookPages = document.createElement("div");
+    bookPages.classList.add("book-pages");
+    bookCard.appendChild(bookPages);
+    bookPages.textContent = `Pages: ${book.page}`;
+}
+
+function addReadStatus(bookCard, book) {
+    let bookReadStatus = document.createElement("div");
+    bookReadStatus.classList.add("book-read-status");
+    bookCard.appendChild(bookReadStatus);
+    if (book.read === true) {
+        bookReadStatus.textContent = "Read: Yes";
+    }
+    else {
+        bookReadStatus.textContent = "Read: No";
+    }
+}
+
+function openNewBookForm() {
     
 }
+
+const button = document.querySelector("#new-book-button");
+button.addEventListener("click", () => {
+    const newBookFormContainer = document.querySelector("#form-container");
+    const newBookForm = document.createElement("form");
+    newBookForm.setAttribute("id", "new-book-form");
+    newBookFormContainer.appendChild(newBookForm);
+    createLabelandInput(newBookForm);
+
+
+
+})
+
+function createLabelandInput(newBookForm) {
+    const label = document.createElement("label");
+    label.htmlFor = "title";
+    newBookForm.appendChild(label);
+    label.textContent = "Title";
+
+    const input = document.createElement("input");
+    input.type = "text";
+    input.setAttribute("id", "title");
+    newBookForm.appendChild(input);
+}
+
+
